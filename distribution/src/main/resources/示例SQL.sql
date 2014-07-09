a94fcd752a71b04167f5b5e10a5439b830f813a1
zqq90 <zqq_90@163.com>
2014-07-09 14:49:21 +0800

    完善例子打包

--- distribution/src/main/resources/示例SQL.sql
+++ distribution/src/main/resources/示例SQL.sql
@@ -2,11 +2,11 @@
 -- use jfinal_demo;
 
 CREATE TABLE `blog` (
-  `id` int(11) NOT NULL auto_increment,
-  `title` varchar(200) NOT NULL,
-  `content` mediumtext NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='博客';
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `title` varchar(200) NOT NULL COMMENT '标题',
+  `content` mediumtext NOT NULL COMMENT '内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='博客';
 
 
 INSERT INTO `blog` VALUES
